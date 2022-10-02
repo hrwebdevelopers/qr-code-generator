e8d80d32b265ba05eb1824a3440fde0ddf5da62c
Hector Romero <hectorromerodev@gmail.com>
2022-10-01 21:29:28 -0700

    upd email

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:hrwebdevelopers.github.io.git main
+git push -f git@github.com:hectorromerodev.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
